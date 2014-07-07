9c747219aff5a9ee5f724d2e797782fdaee59fd9
Janne Parkkila <japskua@gmail.com>
2014-07-07 14:10:42 +0300

    Now the flow works nicely.

--- ieee-parser.py
+++ ieee-parser.py
@@ -1,13 +1,33 @@
 import xml.etree.ElementTree as ET
 from ieeeprocessor import IEEEProcessor
+from ieeesearch import SearchEngine
 import sys
 
 
 def main():
 
-    tree = ET.parse('searchresult.xml')
-    root = tree.getroot()
+    # Initialize the search engine
+    se = SearchEngine()
 
+    # Define the query terms
+    se.set_query("games AND interoperability")
+    # Set the paging
+    se.set_paging(10, 1)
+    # Perform the query
+    se.perform_query()
+    #se.display_results()
+    # Get the results
+    res = se.get_results()
+
+    """ <<--- Okay, now we continue with parsing the results into wanted format --->> """
+
+    # Construct the tree by parsin the XML file
+    #tree = ET.parse(res)
+    # Then, get the root of the tree
+    root = ET.fromstring(res)
+    #tree = ET.parse('searchresult.xml')
+
+    # Then, start processing the information
     processor = IEEEProcessor()
     processor.ProcessSearchResults(root)
 
--- ieeesearch.py
+++ ieeesearch.py
@@ -4,7 +4,15 @@ import urllib.request, urllib.parse
 
 
 class SearchEngine:
+    """
+    The SearchEngine class is in charge of constructing and performing the queries
+    at the IEEE Service.
+    """
     def __init__(self):
+        """
+        The Constructor for the class
+        :return:
+        """
         # The constructor function
         self._contents = ""
         self._url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp"
@@ -20,7 +28,11 @@ class SearchEngine:
         self._hc = 0
         self._rs = 0
 
-    def query(self):
+    def perform_query(self):
+        """
+        Creates the query and runs it at the IEEE server
+        :return:
+        """
         # Construct the query
         self.__construct_query()
 
@@ -34,12 +46,27 @@ class SearchEngine:
         self._contents = byte_contents.decode("utf8")
 
     def display_results(self):
+        """
+        Displays the results received
+        :return:
+        """
         print(self._contents)
 
     def set_query(self, text):
+        """
+        Sets the query text
+        :param text:
+        :return:
+        """
         self._queryText = urllib.parse.quote(text)
 
     def set_paging(self, number_to_fetch, sequence_start):
+        """
+        Sets the paging settings for the query to be performed
+        :param number_to_fetch: The number of items to fetch (1-100), maximum is 100 per query
+        :param sequence_start: The sequence starting position (1 is the first, 50 means 50th results, etc...)
+        :return: Returns nothing
+        """
         if number_to_fetch != 0:
             self._hc = number_to_fetch
         if sequence_start != 0:
@@ -47,10 +74,18 @@ class SearchEngine:
 
     # This is for debugging only!
     def __displayQuery(self):
+        """
+        Displays the constructed query
+        :return:
+        """
         print(self._fullQueryString)
 
     # Constructs the query
     def __construct_query(self):
+        """
+        Constructs the query to be run at IEEE server
+        :return:
+        """
         # Construct the query string
         # First, just take the url
         self._fullQueryString = self._url
@@ -78,10 +113,24 @@ class SearchEngine:
             self._fullQueryString += "&rs=" + str(self._rs)
 
     def get_results(self):
+        """
+        Gets the contents returned from the query
+        :return: an XML string containing the result information
+        """
         return self._contents
 
     # Sets the filtering, if wanted
     def set_filtering(self, oa=0, pn=None, pys=None, pye=None, pu=None, ctype=None):
+        """
+        Sets the filtering parameters for the query data
+        :param oa: Open Access Only (1 = true, blank otherwise)
+        :param pn: Publication number
+        :param pys: Start value of the publication year to restrict the results by
+        :param pye: End value of the publication year to restrict the results by
+        :param pu: Publisher, options are IEEE/AIP/IET/AVS/IBM
+        :param ctype: Content type: Conferences/Journals/Books/Early Access/Standards/Educational Courses
+        :return:
+        """
         # Set the filtering terms
         if oa == 1:
             self._oa = 1
@@ -96,9 +145,10 @@ class SearchEngine:
         if ctype is not None:
             self._ctype = ctype
 
+
 se = SearchEngine()
 se.set_query("games AND interoperability")
 se.set_paging(2, 1)
-se.query()
+se.perform_query()
 se.display_results()
 res = se.get_results()
